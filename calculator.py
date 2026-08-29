c0bcafe064eb0d3c1646f3f940b588d83ad11368
VickySS2020 <A22216352@alumnos.uady.mx>
2026-08-29 17:34:11 -0600

    Experimento #3_Add new feature

--- calculator.py
+++ calculator.py
@@ -6,3 +6,5 @@ def multiply(a, b):
  return a * b
 def divide(a, b):
  return a / b
+def modulo(a, b):
+ return a % b
--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,4 @@
-from calculator import add, subtract, multiply, divide
+from calculator import add, subtract, multiply, divide, modulo
 def test_add():
  assert add(2, 3) == 5
 def test_subtract():
@@ -7,3 +7,6 @@ def test_multiply():
  assert multiply(4, 3) == 12
 def test_divide():
  assert divide(10, 2) == 5
+def test_modulo():
+ assert modulo(10, 3) == 1
+
